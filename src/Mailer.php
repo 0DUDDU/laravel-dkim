e0ac31aa174b70cda4e5d4ad6b2276a464415dfc
lucas duval <duval.lucas@hotmail.fr>
2020-09-13 13:38:09 +0200

    allow relative path

--- src/Mailer.php
+++ src/Mailer.php
@@ -16,15 +16,23 @@ class Mailer extends \Illuminate\Mail\Mailer
         if (! empty($this->from['address'])) {
             $message->from($this->from['address'], $this->from['name']);
         }
+        $key = config('mail.dkim_private_key');
+        $isAbsoluteLinuxPath = strpos($key, '/') === 0;
+        $isAbsoluteWindowsPath = strpos($key, ':\\') !== false;
+
+        $isRelativePath = !$isAbsoluteLinuxPath && !$isAbsoluteWindowsPath;
+
+        $key =  $isRelativePath ? $this->app->basePath($key) : $key;
 
         if (in_array(strtolower(config('mail.default')), ['smtp', 'sendmail', 'log'])) {
-            if (config('mail.dkim_private_key') && file_exists(config('mail.dkim_private_key'))) {
+            if (config('mail.dkim_private_key') && file_exists($key)) {
                 if (config('mail.dkim_selector') && config('mail.dkim_domain')) {
-                    $message->attachDkim(config('mail.dkim_selector'), config('mail.dkim_domain'), file_get_contents(config('mail.dkim_private_key')), config('mail.dkim_passphrase'));
+
+                    $message->attachDkim(config('mail.dkim_selector'), config('mail.dkim_domain'), file_get_contents($key), config('mail.dkim_passphrase'));
                 }
             }
         }
-        
+
         return $message;
     }
 
